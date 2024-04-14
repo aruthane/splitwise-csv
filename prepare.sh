b704404fd6a79b401bf29ad049cdb8be591d3f74
Carlin St Pierre <carlin.stpierre@gmail.com>
2024-04-14 11:02:09 +1000

    Strip ANZ prefixes from transactions in prepare script

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -exuf
+
+sed -i 's/VISA DEBIT PURCHASE CARD [[:digit:]]\+ //g' ANZ.csv
+sed -i 's/ANZ INTERNET BANKING PAYMENT [[:digit:]]\+ TO //g' ANZ.csv
+sed -i 's/ANZ MOBILE BANKING PAYMENT [[:digit:]]\+ TO //g' ANZ.csv
