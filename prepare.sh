4830c2e65d2bf7753ddd35bf6314033721d52a74
Carlin St Pierre <carlin.stpierre@gmail.com>
2024-05-11 13:07:08 +1000

    Remove more ANZ prefixes

--- prepare.sh
+++ prepare.sh
@@ -5,3 +5,4 @@ set -exuf
 sed -i 's/VISA DEBIT PURCHASE CARD [[:digit:]]\+ //g' ANZ.csv
 sed -i 's/ANZ INTERNET BANKING PAYMENT [[:digit:]]\+ TO //g' ANZ.csv
 sed -i 's/ANZ MOBILE BANKING PAYMENT [[:digit:]]\+ TO //g' ANZ.csv
+sed -i 's/ANZ MOBILE BANKING BPAY DEFT //g' ANZ.csv
